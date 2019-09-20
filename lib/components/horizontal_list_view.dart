947ee4d81ff48ec83c519020f3d84b10af5136e5
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2019-09-20 19:02:42 +0200

    12 - 12. Flutter e-commerce app - Horizontal list view part 2

--- lib/components/horizontal_list_view.dart
+++ lib/components/horizontal_list_view.dart
@@ -9,29 +9,68 @@ class HorizontalList extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-          new Icon(Icons.help),
-          new Icon(Icons.help),
-          new Icon(Icons.help),
+          Category(
+            imageLocation: 'images/cats/accessories.png',
+            imageCaption: 'Accessories',
+          ),
+          Category(
+            imageLocation: 'images/cats/dress.png',
+            imageCaption: 'Dress',
+          ),
+          Category(
+            imageLocation: 'images/cats/formal.png',
+            imageCaption: 'Formal',
+          ),
+          Category(
+            imageLocation: 'images/cats/informal.png',
+            imageCaption: 'Informal',
+          ),
+          Category(
+            imageLocation: 'images/cats/jeans.png',
+            imageCaption: 'Jeans',
+          ),
+          Category(
+            imageLocation: 'images/cats/shoe.png',
+            imageCaption: 'shoe',
+          ),
+          Category(
+            imageLocation: 'images/cats/tshirt.png',
+            imageCaption: 'tshirt',
+          ),
         ],
       ),
     );
   }
 }
-class Category extends StatelessWidget
-{
+
+class Category extends StatelessWidget {
   final String imageLocation;
   final String imageCaption;
 
-  Category({Key key, this.imageLocation, this.imageCaption}) : super(key: key);
+  const Category({Key key, this.imageLocation, this.imageCaption})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(2.0),
       child: InkWell(
-        onTap: (){},
-        child: ListTile(
-          title: Image.asset(imageLocation),
-          subtitle: Text(imageCaption),
+        onTap: () {},
+        child: Container(
+          width: 100.0,
+          child: ListTile(
+            title: Image.asset(
+              imageLocation,
+              height: 100.0,
+              width: 80.0,
+            ),
+            subtitle: Container(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  imageCaption,
+                  style: TextStyle(fontSize: 12.0),
+                )),
+          ),
         ),
       ),
     );
