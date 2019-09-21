86d1da8699627946f0ea3ecad8111799e12ca9c0
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2019-09-21 13:09:22 +0200

    18 - 18  Flutter e-commerce app - product details continuation

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -59,49 +59,97 @@ class _ProductDetailsState extends State<ProductDetails> {
           ),
           Row(
             children: <Widget>[
-              buildExpandedButton('Size', Icons.arrow_drop_down),
-              buildExpandedButton('Color', Icons.arrow_drop_down),
-              buildExpandedButton('Qty', Icons.arrow_drop_down),
+              buildExpandedButton('Size', Icons.arrow_drop_down, onClose: () {
+                Navigator.of(context).pop(context);
+              }),
+              buildExpandedButton('Color', Icons.arrow_drop_down,
+                  onClose: () {}),
+              buildExpandedButton('Qty', Icons.arrow_drop_down, onClose: () {}),
             ],
           ),
           Row(
             children: <Widget>[
               buildBuyButton('Buy Now'),
               IconButton(
-                icon: Icon(Icons.add_shopping_cart, color: Colors.red,),
-                onPressed: (){},
+                icon: Icon(
+                  Icons.add_shopping_cart,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
               ),
               IconButton(
-                icon: Icon(Icons.favorite, color: Colors.red,),
-                onPressed: (){},
+                icon: Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
               ),
             ],
-          )
+          ),
+          Divider(),
+          ListTile(
+              title: Text('Product Details'),
+              subtitle: Text(
+                  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")),
+          Divider(),
+          buildSpecsRow('Product name', 'Blazer'),
+          buildSpecsRow('Product brand', 'X'),
+          buildSpecsRow('Product condition', 'NEW'),
         ],
       ),
     );
   }
 
-  Expanded buildExpandedButton(String text, IconData icon) {
-    return Expanded(
-              child: MaterialButton(
-                onPressed: () {},
-                color: Colors.white,
-                textColor: Colors.grey,
-                elevation: 0.2,
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(text),
-                    ),
-                    Expanded(
-                      child: Icon(icon),
-                    ),
-                  ],
-                ),
-              ),
-            );
+  Row buildSpecsRow(String spec, String value) {
+    return Row(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(12.0, 5.0, 5.0, 5.0),
+              child:
+                  Text(spec, style: TextStyle(color: Colors.grey)),
+            ),
+            Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Text(value),
+            )
+          ],
+        );
   }
+
+  Expanded buildExpandedButton(String text, IconData icon, {Function onClose}) {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: Text(text),
+                    content: Text('Select the ' + text),
+                    actions: <Widget>[
+                      MaterialButton(
+                        onPressed: onClose,
+                        child: Text('Close'),
+                      )
+                    ],
+                  ));
+        },
+        color: Colors.white,
+        textColor: Colors.grey,
+        elevation: 0.2,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Text(text),
+            ),
+            Expanded(
+              child: Icon(icon),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   Expanded buildBuyButton(String text) {
     return Expanded(
       child: MaterialButton(
@@ -109,7 +157,10 @@ class _ProductDetailsState extends State<ProductDetails> {
         color: Colors.red,
         textColor: Colors.white,
         elevation: 0.2,
-        child: Text(text, textAlign: TextAlign.center,),
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
